27f30d15c6190e17b919b75af5a462fe059b34ed
Pooja-Mehra9921 <159164676+Pooja-Mehra9921@users.noreply.github.com>
2024-06-16 23:21:10 +0530

    make main content section with javascript

---
+++ News Search App/script.js
@@ -0,0 +1,47 @@
+const apikey='c4d277bfe41b4f6797e10d76f87bf7ff'
+
+const blogContainer = document.getElementById("blog-container");
+
+async function fetchrandom(){
+    try{
+const apiURL = `https://newsapi.org/v2/top-headlines?country=us&pageSize=10&apikey=${apikey}`
+const response = await fetch(apiURL)
+const data = await response.json()
+return data.articles;
+
+    }catch(error){
+   console.error("Error fetching random news", error);
+    }
+    return[]
+}
+
+function displayBlogs(articles){
+    blogContainer.innerHTML = ""
+    articles.forEach((article)=>{
+const blogCard = document.createElement("div")
+blogCard.classList.add("blog-card")
+const img = document.createElement("img")
+img.src = article.urlToImage
+img.alt= article.title
+const title = document.createElement("h2")
+title.textContent = article.title
+const description = document.createElement("p")
+description.textContent = article.description
+
+blogCard.appendChild(img)
+blogCard.appendChild(title)
+blogCard.appendChild(description)
+blogContainer.appendChild(blogCard)
+
+    })
+}
+
+(async()=>{
+    try{
+const articles= await fetchrandom();
+displayBlogs(articles);
+    }catch(error){
+        console.error("Error fetching random news", error);
+
+    }
+})
