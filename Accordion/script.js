6391cf08cc737e7944245f3f30a4ea1810c78a5b
Pooja-Mehra9921 <io6.poojamehra@gmail.com>
2025-01-02 18:03:04 +0530

    adding functionality in this

---
+++ Accordion/script.js
@@ -0,0 +1,15 @@
+const firstQuestion = document.querySelector(".first-qustion");
+const secondQuestion = document.querySelector(".second-qustion");
+const thirdQuestion = document.querySelector(".third-qustion");
+const fourthQuestion = document.querySelector(".fourth-qustion");
+const fifthQuestion = document.querySelector(".fifth-qustion");
+const sixthQuestion = document.querySelector(".sixth-qustion");
+
+
+
+
+function handleFirstQuetion(){
+    console.log("------------first");
+}
+
+firstQuestion.addEventListener("click", handleFirstQuetion);
