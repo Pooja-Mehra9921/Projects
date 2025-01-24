c5397aceeffda36ca745d01c4093f6f2d46244e4
Pooja-Mehra9921 <io6.poojamehra@gmail.com>
2025-01-24 18:01:14 +0530

    adding validations

--- Flash cards/App.jsx
+++ Flash cards/App.jsx
@@ -1,16 +1,9 @@
 import React, { useState } from "react";
 import { Line } from "rc-progress";
+import Card from "./component/Card";
+import Questions from "../Flash cards/Users"
 
-const questions = [{
-    id: 0,
-    question: "what is the difference between var, let, const?",
-    answer: "In JavaScript, var, let, and const are all used to declare variables, but they differ in their scope, use, and hoisting."
-},
-{
-    id: 1,
-    question: "what is javascript",
-    answer: "avaScript is a programming language that allows developers to make interactive web pages. It's a client-side scripting language that's a core technology of the World Wide Web."
-}]
+
 
 const App = () => {
 
@@ -47,18 +40,15 @@ const handlepercentage = () => {
             />
             <div className="question-numbers">{percentage}/20</div>
           </div>
-
-          <div className="card-content-conatiner">
-            <div className="card">
-        <p>hello {questions[0].question}</p>
-        <p>hello {questions[0].answer}</p>
-            </div>
+{Questions.map((e, index)=>{
+    return <Card key={index} question = {e.question} answer ={e.answer} />;
+})};
+          
 
 
             <button onClick={handlepercentage}>increase</button>
           </div>
         </div>
-      </div>
     </>
   );
 };
---
+++ Flash cards/Users.js
@@ -0,0 +1,106 @@
+const questions = [
+  {
+    id: 0,
+    question: "what is the difference between var, let, const?",
+    answer:
+      "In JavaScript, var, let, and const are all used to declare variables, but they differ in their scope, use, and hoisting.",
+  },
+  {
+    id: 1,
+    question: "what is javascript",
+    answer:
+      "avaScript is a programming language that allows developers to make interactive web pages. It's a client-side scripting language that's a core technology of the World Wide Web.",
+  },
+  {
+    id: 2,
+    question: "Question 3",
+    answer:
+      "In JavaScript, var, let, and const are all used to declare variables, but they differ in their scope, use, and hoisting.",
+  },
+  {
+    id: 3,
+    question: "Question 4",
+    answer:
+      "In JavaScript, var, let, and const are all used to declare variables, but they differ in their scope, use, and hoisting.",
+  },{
+    id: 4,
+    question: "Question 5",
+    answer:
+      "In JavaScript, var, let, and const are all used to declare variables, but they differ in their scope, use, and hoisting.",
+  },{
+    id: 5,
+    question: "Question 6",
+    answer:
+      "In JavaScript, var, let, and const are all used to declare variables, but they differ in their scope, use, and hoisting.",
+  },{
+    id: 6,
+    question: "Question 7",
+    answer:
+      "In JavaScript, var, let, and const are all used to declare variables, but they differ in their scope, use, and hoisting.",
+  },{
+    id: 7,
+    question: "Question 8",
+    answer:
+      "In JavaScript, var, let, and const are all used to declare variables, but they differ in their scope, use, and hoisting.",
+  },{
+    id: 8,
+    question: "Question 9",
+    answer:
+      "In JavaScript, var, let, and const are all used to declare variables, but they differ in their scope, use, and hoisting.",
+  },{
+    id: 9,
+    question: "Question 10",
+    answer:
+      "In JavaScript, var, let, and const are all used to declare variables, but they differ in their scope, use, and hoisting.",
+  },{
+    id: 10,
+    question: "Question 11",
+    answer:
+      "In JavaScript, var, let, and const are all used to declare variables, but they differ in their scope, use, and hoisting.",
+  },{
+    id: 11,
+    question: "Question 12",
+    answer:
+      "In JavaScript, var, let, and const are all used to declare variables, but they differ in their scope, use, and hoisting.",
+  },{
+    id: 12,
+    question: "Question 13",
+    answer:
+      "In JavaScript, var, let, and const are all used to declare variables, but they differ in their scope, use, and hoisting.",
+  },{
+    id: 13,
+    question: "Question 14",
+    answer:
+      "In JavaScript, var, let, and const are all used to declare variables, but they differ in their scope, use, and hoisting.",
+  },{
+    id: 14,
+    question: "Question 15",
+    answer:
+      "In JavaScript, var, let, and const are all used to declare variables, but they differ in their scope, use, and hoisting.",
+  },{
+    id: 15,
+    question: "Question 16",
+    answer:
+      "In JavaScript, var, let, and const are all used to declare variables, but they differ in their scope, use, and hoisting.",
+  },{
+    id: 16,
+    question: "Question 17",
+    answer:
+      "In JavaScript, var, let, and const are all used to declare variables, but they differ in their scope, use, and hoisting.",
+  },{
+    id: 17,
+    question: "Question 18",
+    answer:
+      "In JavaScript, var, let, and const are all used to declare variables, but they differ in their scope, use, and hoisting.",
+  },{
+    id: 18,
+    question: "Question 19",
+    answer:
+      "In JavaScript, var, let, and const are all used to declare variables, but they differ in their scope, use, and hoisting.",
+  },{
+    id: 19,
+    question: "Question 20",
+    answer:
+      "In JavaScript, var, let, and const are all used to declare variables, but they differ in their scope, use, and hoisting.",
+  },
+];
