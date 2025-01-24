de499c673a18ca583b26cb72ddf2dadd5013175a
Pooja-Mehra9921 <io6.poojamehra@gmail.com>
2025-01-24 17:19:46 +0530

    adding validation in the code

--- Flash cards/App.jsx
+++ Flash cards/App.jsx
@@ -1,6 +1,17 @@
 import React, { useState } from "react";
 import { Line } from "rc-progress";
 
+const questions = [{
+    id: 0,
+    question: "what is the difference between var, let, const?",
+    answer: "In JavaScript, var, let, and const are all used to declare variables, but they differ in their scope, use, and hoisting."
+},
+{
+    id: 1,
+    question: "what is javascript",
+    answer: "avaScript is a programming language that allows developers to make interactive web pages. It's a client-side scripting language that's a core technology of the World Wide Web."
+}]
+
 const App = () => {
 
 const [percentage, setpercentage] = useState(0);
@@ -37,7 +48,13 @@ const handlepercentage = () => {
             <div className="question-numbers">{percentage}/20</div>
           </div>
 
-          <div className="card">
+          <div className="card-content-conatiner">
+            <div className="card">
+        <p>hello {questions[0].question}</p>
+        <p>hello {questions[0].answer}</p>
+            </div>
+
+
             <button onClick={handlepercentage}>increase</button>
           </div>
         </div>
