8488d490692b03cf40ea945eb41a342e62020ce8
Pooja-Mehra9921 <159164676+Pooja-Mehra9921@users.noreply.github.com>
2025-01-26 18:40:36 +0530

    adding responsiveness in the design

--- Portfolio/index.js
+++ Portfolio/index.js
@@ -1,34 +1,30 @@
-const user={
-    name:"",
-    email:"",
-    message:""
+const user = {
+  name: "",
+  email: "",
+  message: "",
 };
 
-const handleSubmitBtn=()=>{
-   
+const handleSubmitBtn = () => {
+  let userName = document.getElementById("user-name").value;
+  let userEmail = document.getElementById("email").value;
+  let userMessage = document.getElementById("write-message").value;
 
-    let userName = document.getElementById("user-name").value;
-    let userEmail = document.getElementById("email").value;
-    let userMessage = document.getElementById("write-message").value;
+  console.log("name-------------", userName);
+  console.log("email-------------", userEmail);
+  console.log("Mesaage-------------", userMessage);
 
-console.log("name-------------",userName);
-console.log("email-------------",userEmail);
-console.log("Mesaage-------------",userMessage);
-
-if(!userName || !userEmail || !userMessage){
+  if (!userName || !userEmail || !userMessage) {
     let errorElement = document.getElementsByClassName("error-div");
-    errorElement.innerHTML = `<p style="color:red;">Please fill all filels</p>`;
-}
+    errorElement.classList.add = "error";
+    return;
+  }
 
-    if(userName && userEmail && userMessage){
-        console.log("message send--");
-        user.name = userName;
-        user.email = userEmail;
-        user.message = userMessage;
-
-    }else{
-        console.warn("please enteter all fields--");
-    }
-
+  if (userName && userEmail && userMessage) {
+    console.log("message send--");
+    user.name = userName;
+    user.email = userEmail;
+    user.message = userMessage;
+  } else {
+    console.warn("please enteter all fields--");
+  }
 };
-
